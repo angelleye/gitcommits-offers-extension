ef1c603fcff1d55dce099976bd46f94caf0b9a17
deepakmaurya <deepakmaurya@hotmail.com>
2019-09-13 10:53:41 +0530

    Lodash and other vulnerable packages updated, Pointing the API to sandbox updated url, Options page removed as no use for now

--- variables.js
+++ variables.js
@@ -1,4 +1,6 @@
 var OPTIONS = {
-    baseurl: "http://gitworkdone.test",
-    apiurl:  "http://gitworkdone.test/api/v1"
+    // baseurl: "http://gitworkdone.test",
+    // apiurl:  "http://gitworkdone.test/api/v1"
+    baseurl: "https://sandbox.app.gitcommits.com",
+    apiurl:  "https://sandbox.app.gitcommits.com/api/v1"
 };
